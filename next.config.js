0c4a7fef2d9afd300d174916999d88764b4d8fa2
Linares Amat, Javier <javier.linares.amat@accenture.com>
2021-03-03 17:35:33 +0100

    Next config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,29 @@
-// const MediaQueryPlugin = require('media-query-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MediaQueryPlugin = require('media-query-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     webpack: (config) => {
-        // config.plugins.push(
-        //     new MiniCssExtractPlugin()
-        // );
-        // config.module.rules.push({
-        //     test: /\.scss$/,
-        //     use: [
-        //         MiniCssExtractPlugin.loader,
-        //         'css-loader',
-        //         MediaQueryPlugin.loader,
-        //         'postcss-loader',
-        //         'sass-loader'
-        //     ]
-        // });
+        config.plugins.push(new MiniCssExtractPlugin());
+        config.plugins.push(
+            new MediaQueryPlugin({
+                include: [
+                    'example'
+                ],
+                queries: {
+                    'screen and (max-width: 20rem)': 'mobile',
+                }
+            })
+        );
+        config.module.rules.push({
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                MediaQueryPlugin.loader,
+                'postcss-loader',
+                'sass-loader'
+            ]
+        });
         return config;
     },
 };
--- pages/_app.js
+++ pages/_app.js
@@ -4,18 +4,6 @@ import '../styles/index.scss'
 
 function MyApp({ Component, pageProps }) {
 
-  useEffect(() => {
-    
-    if (window && window.innerWidth <= 600) {
-      import ( /* webpackChunkName: 'media-mobile' */ '../public/css/mobile.css');
-    }
-    else if (window && window.innerWidth >= 1200) {
-      import ( /* webpackChunkName: 'media-desktop' */ '../public/css/desktop.css');
-    }
-
-
-  }, [])
-
 
     return <Component {...pageProps }
     />
